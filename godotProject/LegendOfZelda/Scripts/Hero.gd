c6ae38d08851de014c406daae0bde2879b94900d
Avery Crespi <averycrespi@gmail.com>
2020-02-15 07:37:12 -0800

    Start overworld tiles

--- godotProject/LegendOfZelda/Scripts/Hero.gd
+++ godotProject/LegendOfZelda/Scripts/Hero.gd
@@ -4,7 +4,7 @@ extends Area2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-onready var pop = get_child(2)
+var pop = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,7 +18,8 @@ func _ready():
 
 func _on_7UP_body_entered(body):
 	if(body.get_name() == "Player"):
-		pop.popup_centered(Vector2(200,200))
+		pop = get_child(2)
+		pop.popup_centered(Vector2(100,50))
 		pop.set_position(get_position()+Vector2(-50, -20))
 	
 
