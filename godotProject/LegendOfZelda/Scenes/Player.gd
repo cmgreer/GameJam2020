28514dd226fee742514c57babc861af6000ab98e
Avery Crespi <averycrespi@gmail.com>
2020-02-14 23:40:09 -0800

    Start Zelda

---
+++ godotProject/LegendOfZelda/Scenes/Player.gd
@@ -0,0 +1,28 @@
+extends Area2D
+
+export var speed = 400
+
+var screen_size
+
+func _ready():
+	screen_size = get_viewport_rect().size
+	
+func _process(delta):
+	# Calculate movement vector.
+	var velocity = Vector2()
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+		
+	# Update position.
+	position += velocity * delta
