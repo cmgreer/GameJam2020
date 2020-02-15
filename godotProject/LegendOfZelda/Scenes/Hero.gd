b61dd39bc2dc546351ee4ad79bfa17067ed0b549
cmgreer <cmgreer@sfu.ca>
2020-02-15 06:20:00 -0800

    started dialog box tests

--- godotProject/LegendOfZelda/Scenes/Hero.gd
+++ godotProject/LegendOfZelda/Scenes/Hero.gd
@@ -4,6 +4,7 @@ extends Area2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+var pop = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -16,5 +17,9 @@ func _ready():
 
 
 func _on_7UP_body_entered(body):
-	print("test")
+	if(body.get_name() == "Player"):
+		pop = get_child(2)
+		pop.popup_centered(Vector2(100,50))
+		pop.set_position(get_position()+Vector2(-50, -20))
+	
 
