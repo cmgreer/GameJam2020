ca74317b4820620e4af9706f7d87816f37f6fa05
ivuckovi@sfu.ca <ivuckovi@sfu.ca>
2020-02-15 17:14:41 -0800

    added broom and character walking sprite

--- godotProject/LegendOfZelda/Scripts/Player.gd
+++ godotProject/LegendOfZelda/Scripts/Player.gd
@@ -4,6 +4,23 @@ export var speed = 400
 
 var velocity = Vector2()
 
+func walk(vel):
+	$Standing.hide()
+	$WalkingBackward.hide()
+	$WalkingForward.hide()
+	$WalkingLeft.hide()
+	$WalkingRight.hide()
+	if(vel.x>0):
+		$WalkingRight.show()
+	elif(vel.x<0):
+		$WalkingLeft.show()
+	elif(vel.y>0):
+		$WalkingForward.show()
+	elif(vel.y<0):
+		$WalkingBackward.show()
+	else:
+		$Standing.show()
+
 func get_input():
 	# Detect up/down/left/right keystate and only move when pressed
 	velocity = Vector2()
@@ -21,4 +38,7 @@ func _physics_process(delta):
 	if(not globalSingleton.player_frozen):
 		get_input()
 		move_and_collide(velocity * delta)
+		walk(velocity)
 		globalSingleton.playerPosition = get_position()
+	else:
+		walk(Vector2.ZERO)
