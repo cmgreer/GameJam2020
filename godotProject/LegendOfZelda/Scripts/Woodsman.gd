865e1f122628f5ab3825b7ef7a71dd5a65b3c7b3
cmgreer <cmgreer@sfu.ca>
2020-02-16 05:33:32 -0800

    included tinkerer artwork

--- godotProject/LegendOfZelda/Scripts/Woodsman.gd
+++ godotProject/LegendOfZelda/Scripts/Woodsman.gd
@@ -41,6 +41,7 @@ func _on_PopupPanel_popup_hide():
 
 
 func _on_ChoicesBox_dialogBoxCheck(boxNum):
+	print(globalSingleton.player_frozen)
 	match dialogLevel:
 		1:
 			match boxNum:
@@ -68,7 +69,7 @@ func _on_ChoicesBox_dialogBoxCheck(boxNum):
 			match boxNum:
 				0:
 					#good answer, join team, print thank you message
-					join_team()
+					launch_quest()
 				1:
 					#bad answer
 					backout()
@@ -77,16 +78,17 @@ func backout():
 	dialogLevel = 10
 	popReject.popup_centered(Vector2(360,90))
 	popReject.set_position(globalSingleton.playerPosition+Vector2(-178, 80))
-	globalSingleton.character_status[3] = 0
+	globalSingleton.character_status[5] = 0
 
 
 func join_team():
 	dialogLevel = 10
 	popJoin.popup_centered(Vector2(360,90))
 	popJoin.set_position(globalSingleton.playerPosition+Vector2(-178, 80))
-	globalSingleton.character_status[3] = 2
+	globalSingleton.character_status[5] = 2
 
-
+func launch_quest():
+	pass
 
 func _on_RejectPanel_about_to_show():
 	globalSingleton.player_frozen -=1
