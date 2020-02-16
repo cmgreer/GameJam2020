7a8aee36d21425f939d64dd9dc19e0e471131faf
ArgusLi <contactme@chunkeili.com>
2020-02-15 20:23:55 -0800

    finished beast and now working on adding other sceneswo

--- godotProject/Global.gd
+++ godotProject/Global.gd
@@ -5,6 +5,9 @@ extends Node
 #character_status 0==dead/not unlocked, 1==injured, 2==Healthy
 var character_status = [1,0,2,2,2,2,2]
 var character_name = ["Player", "Hunter", "Witch", "Dwarf", "Brad Bart", "Woodsman", "Tinker"]
+var next_scene = 1
+var beast_status = 1 #beast alive
+var witch_potion = 1 #num of witch potions
 
 var player_frozen = false
 
