6fdb2036daadb08fa3f953c465b4be24bea70c08
vucko <ivuckovi@sfu.ca>
2020-02-16 03:23:43 -0800

    Inventory

--- godotProject/TestWorld/Play.gd
+++ godotProject/TestWorld/Play.gd
@@ -3,6 +3,17 @@ extends KinematicBody2D
 export var speed = 400
 
 var velocity = Vector2()
+var escapeMenu = false
+var Items_collected = [0,0,0,0,0]
+var sprites=[]
+
+
+func _draw():
+	sprites.append($Sprite/ItemContainer)
+	for x in range(1,len(Items_collected)):
+		sprites.append($Sprite/ItemContainer.duplicate(15))
+		$Sprite.add_child(sprites[-1])
+		sprites[-1].position+=Vector2(10,0)*x
 
 func walk(vel):
 	$Standing.hide()
@@ -32,13 +43,19 @@ func get_input():
 		velocity.y += 1
 	if Input.is_action_pressed('ui_up'):
 		velocity.y -= 1
+	if Input.is_action_just_pressed('ui_cancel'):
+		$Sprite.visible=!$Sprite.visible
+		globalSingleton.player_frozen=$Sprite.visible
+	
 	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
 	if(not globalSingleton.player_frozen):
 		get_input()
+# warning-ignore:return_value_discarded
 		move_and_collide(velocity * delta)
 		walk(velocity)
 		globalSingleton.playerPosition = get_position()
 	else:
+		get_input()
 		walk(Vector2.ZERO)
