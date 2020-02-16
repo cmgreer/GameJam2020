0874c613c87865a80672b2ef02ab4ffbee424b74
ArgusLi <contactme@chunkeili.com>
2020-02-16 02:45:42 -0800

    whoops with the accidental assign

--- godotProject/OregonTrail/Scripts/ChoicesBox3.gd
+++ godotProject/OregonTrail/Scripts/ChoicesBox3.gd
@@ -19,7 +19,7 @@ func change_scene(): #Unfinished
 	elif globalSingleton.character_status[0]==0: #if player is dead
 		get_tree().change_scene("res://OregonTrail/Scenes/8.tscn")
 	else: #Next Scene
-		if globalSingleton.beast_status = 1: #if beast alive
+		if globalSingleton.beast_status == 1: #if beast alive
 			get_tree().change_scene("res://OregonTrail/Scenes/4.tscn")
 		else:#if beast not alive
 			get_tree().change_scene("res://OregonTrail/Scenes/5.tscn")
